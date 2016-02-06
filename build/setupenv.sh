16e536d837921c69a54fbdd24260d3892156226f
Carsten Fortmann-Grote <carsten.grote@xfel.eu>
2016-02-06 01:58:26 +0100

    Fixing makefile for ubuntu 14.04.

--- build/setupenv.sh
+++ build/setupenv.sh
@@ -2,6 +2,7 @@
 export PYTHONPATH=$PWD/../sw/lib/python2.7/site-packages:$PYTHONPATH
 export PATH=$PWD/../sw/bin:$PATH
 export INCLUDE_PATH=$PWD/../sw/include:$INCLUDE_PATH
+export LD_LIBRARY_PATH=$PWD/../sw/lib:$PWD/../sw/lib64:$LD_LIBRARY_PATH
 
 PYTHON27=`which python2.7`
 PYTHON27_BIN_PATH=`dirname ${PYTHON27}`
