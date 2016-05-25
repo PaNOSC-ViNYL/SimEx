0f28bece6dcce288dc4b4cb143bda8b135dac461
Carsten Fortmann-Grote <carsten.grote@xfel.eu>
2016-05-25 08:48:44 +0200

    Attempt to fix errors reported by travisCI

--- install.sh
+++ install.sh
@@ -21,7 +21,7 @@ echo "Changed dir to $PWD."
 # Some needed environment variables.
 export BOOST_ROOT=/usr/local
 export Boost_NO_SYSTEM_PATHS=ON
-export ARMA_DIR=/usr/local
+export ARMA_DIR=/usr
 
 # Uncomment the next line and specify the install dir for a custom user install.
 #cmake -DCMAKE_INSTALL_PREFIX=$ROOT_DIR $ROOT_DIR
@@ -29,7 +29,7 @@ export ARMA_DIR=/usr/local
 cmake -DDEVELOPER_INSTALL=ON -DCMAKE_INSTALL_PREFIX=$ROOT_DIR $ROOT_DIR
 
 # Build the project.
-make
+make -j8
 
 # Install the project.
 make install
