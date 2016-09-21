faccacc299c17c3a3819b299326f8e68c113b12e
Sergey Yakubov <sergey.yakubov@desy.de>
2016-09-21 11:14:32 +0200

    added Dockerfile and scripts

---
+++ Docker/simex_install.sh
@@ -0,0 +1,49 @@
+URL=https://github.com/eucall-software/simex_platform/archive/develop.zip
+#
+cd /opt
+
+wget $URL
+unzip develop.zip
+rm develop.zip
+cd simex_platform-develop
+
+
+export BOOST_ROOT=/opt/boost
+export Boost_NO_SYSTEM_PATHS=ON
+#export ARMA_DIR=/usr
+
+export PATH=/opt/miniconda2/bin:$PATH
+export HDF5_ROOT=/opt/miniconda2
+
+ROOT_DIR=/opt/simex_platform
+mkdir -p $ROOT_DIR
+# Create new build dir and cd into it.
+
+mkdir -v build
+cd build
+
+# Uncomment the next line and specify the install dir for a custom user install.
+#cmake -DCMAKE_INSTALL_PREFIX=$ROOT_DIR $ROOT_DIR
+# Uncomment the next line and specify the install dir for a developer install.
+cmake -DSRW_OPTIMIZED=ON -DDEVELOPER_INSTALL=OFF -DCMAKE_INSTALL_PREFIX=$ROOT_DIR $ROOT_DIR ..
+
+chmod og+rwX -R $ROOT_DIR
+
+# Build the project.
+make -j8
+
+
+# Install the project.
+make install
+cd ../..
+
+rm -rf simex_platform-develop
+
+
+#remove tests?
+rm -rf $ROOT_DIR/Tests
+
+echo "source /opt/simex_platform/bin/simex_vars.sh" > /etc/profile.d/scripts-simex.sh && \
+	chmod 755 /etc/profile.d/scripts-simex.sh
+chmod og+rwX -R /opt/simex_platform
+
---
+++ Docker/simex_install_devel.sh
@@ -0,0 +1,49 @@
+URL=https://github.com/eucall-software/simex_platform.git
+cd /opt
+
+wget https://github.com/github/git-lfs/releases/download/v1.1.2/git-lfs-linux-amd64-1.1.2.tar.gz
+mkdir -p $HOME/bin
+tar xvfz git-lfs-linux-amd64-1.1.2.tar.gz
+mv git-lfs-1.1.2/git-lfs $HOME/bin/git-lfs
+export PATH=$PATH:$HOME/bin/
+
+git clone -b develop $URL
+#git clone -b mpi_multifiles $URL
+cd simex_platform
+git lfs pull
+
+export BOOST_ROOT=/opt/boost
+export Boost_NO_SYSTEM_PATHS=ON
+#export ARMA_DIR=/usr
+
+export PATH=/opt/miniconda2/bin:$PATH
+export HDF5_ROOT=/opt/miniconda2
+
+ROOT_DIR=/opt/simex_platform
+
+# Create new build dir and cd into it.
+
+mkdir -v build
+cd build
+
+# Uncomment the next line and specify the install dir for a custom user install.
+#cmake -DCMAKE_INSTALL_PREFIX=$ROOT_DIR $ROOT_DIR
+# Uncomment the next line and specify the install dir for a developer install.
+cmake -DSRW_OPTIMIZED=ON -DDEVELOPER_INSTALL=ON -DCMAKE_INSTALL_PREFIX=$ROOT_DIR $ROOT_DIR
+
+chmod og+rwX -R $ROOT_DIR
+
+# Build the project.
+make -j8
+
+
+# Install the project.
+make install
+cd ..
+
+rm -rf build
+
+echo "source /opt/simex_platform/bin/simex_vars.sh" > /etc/profile.d/scripts-simex.sh && \
+	chmod 755 /etc/profile.d/scripts-simex.sh
+chmod og+rwX -R /opt/simex_platform
+
