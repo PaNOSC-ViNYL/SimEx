1c7f27ba593790d35cbd6f121bc9bc163f93e6bc
Carsten Fortmann-Grote <carsten.grote@xfel.eu>
2016-10-11 14:15:35 +0200

    Adding doc to cmake system.

--- install.sh
+++ install.sh
@@ -26,11 +26,10 @@ export ARMA_DIR=/usr
 # Uncomment the next line and specify the install dir for a custom user install.
 #cmake -DCMAKE_INSTALL_PREFIX=$ROOT_DIR $ROOT_DIR
 # Uncomment the next line and specify the install dir for a developer install.
-cmake -DSRW_OPTIMIZED=ON -DDEVELOPER_INSTALL=ON -DCMAKE_INSTALL_PREFIX=$ROOT_DIR $ROOT_DIR
+cmake -DSRW_OPTIMIZED=ON -DBUILD_DOC=ON -DDEVELOPER_INSTALL=ON -DCMAKE_INSTALL_PREFIX=$ROOT_DIR $ROOT_DIR
 
 # Build the project.
 make -j8
-
 
 # Install the project.
 make install
