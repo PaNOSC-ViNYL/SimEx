36832ecea0b9125e0517844e4899a95f714a1e75
Sergey Yakubov <sergey.yakubov@desy.de>
2016-11-02 15:26:50 +0100

    automated docs

---
+++ TravisCI/build_and_test_simex.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+set -e # Exit with nonzero exit code if anything fails
+
+# load git lfs files
+git lfs pull
+
+# build & install
+mkdir build
+cd build
+cmake -DCMAKE_INSTALL_PREFIX=$VIRTUAL_ENV -DSRW_OPTIMIZED=ON ..
+make
+make install
+
+# build & install docs
+make docs install
+
+# unit tests
+cd $VIRTUAL_ENV/Tests/python/unittest/
+python Test.py -v
+
+# Test doc.
+cd $VIRTUAL_ENV/Tests/doc
+python Test.py -v
+
+# functional tests
+#cd $VIRTUAL_ENV/Tests/python/functest/
+#python Test.py -v
