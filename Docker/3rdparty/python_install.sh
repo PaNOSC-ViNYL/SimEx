eee9d7340df0014a425404584495b7e9621ede50
Sergey Yakubov <sergey.yakubov@desy.de>
2016-12-20 13:06:46 +0100

    added pyqt to dockerfiles, --bind-to none to mpi command

--- Docker/3rdparty/python_install.sh
+++ Docker/3rdparty/python_install.sh
@@ -22,6 +22,7 @@ conda config --set always_yes True
 conda install --no-deps nomkl numpy Cython matplotlib six scipy dateutil pyparsing cycler openblas libpng
 conda install --no-deps libgfortran=1 
 conda install --no-deps -c anaconda biopython=1.67
+conda  install --no-update-deps pyqt
 $PREFIX/bin/pip install periodictable mpi4py dill
 #yum install -y hdf5-devel
 ## clean to reduce image size
