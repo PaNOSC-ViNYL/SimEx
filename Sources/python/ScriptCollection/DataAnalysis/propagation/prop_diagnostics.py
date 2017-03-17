cd6394e4b1466cefdc40d0bd5e588223ed9fe310
Carsten Fortmann-Grote <carsten.grote@xfel.eu>
2017-03-17 11:47:04 +0100

    Updating diagnostic script for prop.

--- Sources/python/ScriptCollection/DataAnalysis/propagation/prop_diagnostics.py
+++ Sources/python/ScriptCollection/DataAnalysis/propagation/prop_diagnostics.py
@@ -33,17 +33,30 @@ from wpg.useful_code.wfrutils import plot_wfront
 
 
 ### TODO
+# Plot phase distribution
 # Plot average and rms intensity distribution over given patterns.
 
 def plot(wavefront):
 
     if args._do_intensity_distribution:
-        plot_wfront(wavefront, title_fig='',
-            isHlog=False, isVlog=False,
-            i_x_min=1e-5, i_y_min=1e-5,
-            orient='x', onePlot=True)
-        pyplot.colorbar()
+        #plot_wfront(wavefront, title_fig='',
+            #isHlog=False, isVlog=False,
+            #i_x_min=1e-5, i_y_min=1e-5,
+            #orient='x', onePlot=True)
+        #pyplot.colorbar()
         plot_t_wf(wavefront)
+
+    if args._do_spectra:
+        spectrum0 = wavefront.custom_fields['misc']['spectrum0']
+        spectrum1 = wavefront.custom_fields['misc']['spectrum1']
+
+        pyplot.plot(spectrum0[:,0],spectrum0[:,1], label="initial")
+        pyplot.plot(spectrum1[:,0],spectrum1[:,1], label="final")
+        pyplot.xlabel("Energy (eV)")
+        pyplot.ylabel("Power spectrum (normalized)")
+        pyplot.legend()
+
+        pyplot.show()
 
     if args._do_phase_distribution:
         pyplot.imshow(wavefront.get_phase(slice_number=0,polarization='horizontal'), cmap='hsv')
@@ -84,22 +97,29 @@ if __name__ == "__main__":
                         "--intensity",
                         action="store_true",
                         dest="_do_intensity_distribution",
-                        default=False,
+                        default=True,
                         help="Plot the intensity distribution in x-y.")
 
     parser.add_argument("-R",
                         "--reciprocal",
                         action="store_true",
                         dest="_do_qspace_intensity",
-                        default=False,
+                        default=True,
                         help="Plot the intensity distribution in qx-qy.")
 
     parser.add_argument("-P",
                         "--phase",
                         action="store_true",
                         dest="_do_phase_distribution",
-                        default=False,
+                        default=True,
                         help="Plot the phase distribution in x-y.")
+
+    parser.add_argument("-S",
+                        "--spectrum",
+                        action="store_true",
+                        dest="_do_spectra",
+                        default=True,
+                        help="Plot the power spectra (before and after propagation.")
 
 
 
