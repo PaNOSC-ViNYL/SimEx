dbb0e73c5de493a48ab76aa81fe2ccec8a89fd9a
Carsten Fortmann-Grote <carsten.grote@xfel.eu>
2017-08-29 11:52:31 +0200

    Attempting to fix build failure on travis (lost feature in sphinx?)

--- Sources/doc/source/conf.py
+++ Sources/doc/source/conf.py
@@ -118,9 +118,9 @@ breathe_default_project = "simex"
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
-html_theme_options = {
-        "stickysidebar" : "true",
-        }
+#html_theme_options = {
+        #"stickysidebar" : "true",
+        #}
 
 # Add any paths that contain custom themes here, relative to this directory.
 #html_theme_path = []
