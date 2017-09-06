f5a079850a617258bc70ec902d48606adf39bb51
Carsten Fortmann-Grote <carsten.grote@xfel.eu>
2017-09-06 14:42:40 +0200

    Adding nunba as required py lib.

--- CMake/FindPythonModules.cmake
+++ CMake/FindPythonModules.cmake
@@ -1,11 +1,11 @@
 function(find_python_module module)
     set (PYTHON_EXECUTABLE python)
-    execute_process(COMMAND "${PYTHON_EXECUTABLE}" "-c" 
+    execute_process(COMMAND "${PYTHON_EXECUTABLE}" "-c"
 	"import ${module};"
-	RESULT_VARIABLE _${module}_status 
+	RESULT_VARIABLE _${module}_status
 	ERROR_QUIET)
 	if(NOT _${module}_status)
-         message ("-- Python module ${module} is found")				
+         message ("-- Python module ${module} is found")
 	else(NOT _${module}_status)
 	    if(ARGC GREATER 1 AND ARGV1 STREQUAL "REQUIRED")
 	        message (FATAL_ERROR "-- Python module ${module} is not found")
