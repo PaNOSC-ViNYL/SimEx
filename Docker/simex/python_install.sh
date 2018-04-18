ec22866efa8382133c1fba039da017412b875398
Chuck <yoon82@slac.stanford.edu>
2018-04-18 15:54:03 +0200

    fixed dockerfile and added miniconda3

---
+++ Docker/simex/python_install.sh
@@ -0,0 +1,47 @@
+#!/usr/bin/env bash
+
+export myminiconda=Miniconda3-4.4.10-Linux-x86_64.sh
+URL=https://repo.continuum.io/miniconda/$myminiconda
+
+# Install dir
+PREFIX=/opt/miniconda
+
+# Download Miniconda
+wget -q $URL
+
+# Extract packages
+
+chmod +x $myminiconda
+./$myminiconda -b -p $PREFIX
+rm ./$myminiconda
+cd $PREFIX
+
+# config
+export PATH=$PREFIX/bin:$PATH
+conda config --set always_yes True
+
+# pip cannot install pyqt
+conda install --no-update-deps pyqt=4
+
+# delete tests
+find . -type d -name tests -depth -exec rm -rf {} \;
+find . -type d -name test -depth -exec rm -rf {} \;
+
+conda clean --tarballs
+
+# install requirements from file (in given order)
+#$PREFIX/bin/pip install -r /opt/requirements.txt
+xargs -L 1 pip install < /opt/requirements.txt
+
+# remove .pyc
+find . -name \__pycache__ -depth -exec rm -rf {} \;
+find . -name "*.pyc" -exec rm -rf {} \;
+
+
+# remove cache
+rm -r pkgs/*
+rm -rf /root/.cache/pip
+
+
+echo "export PATH=/opt/miniconda/bin:${PATH}" >> /etc/profile.d/scripts-path.sh
+
--- Docker/simex/simex_install.sh
+++ Docker/simex/simex_install.sh
@@ -5,7 +5,6 @@ set -e
 BRANCH=develop
 URL=https://github.com/eucall-software/simex_platform/archive/${BRANCH}.zip
 
-
 cd /opt
 
 wget $URL
@@ -13,11 +12,18 @@ unzip ${BRANCH}.zip
 rm ${BRANCH}.zip
 cd simex_platform-${BRANCH}
 
-export PATH=/opt/miniconda2/bin:$PATH
-export HDF5_ROOT=/opt/miniconda2
+export PATH=/opt/miniconda/bin:$PATH
+export HDF5_ROOT=/opt/miniconda
+
+echo "###### DONE unzip ${BRANCH}.zip"
+
+conda install -c intel mkl
+
+echo "##### DONE install mkl"
 
 ROOT_DIR=/opt/simex_platform
 mkdir -p $ROOT_DIR
+
 # Create new build dir and cd into it.
 
 mkdir -v build
@@ -26,23 +32,33 @@ cd build
 # Uncomment the next line and specify the install dir for a custom user install.
 #cmake -DCMAKE_INSTALL_PREFIX=$ROOT_DIR $ROOT_DIR
 # Uncomment the next line and specify the install dir for a developer install.
-cmake -DINSTALL_TESTS=OFF -DSRW_OPTIMIZED=ON -DDEVELOPER_INSTALL=OFF -DCMAKE_INSTALL_PREFIX=$ROOT_DIR $ROOT_DIR ..
+cmake -DXCSITPhotonDetector=OFF \
+      -DGAPDPhotonDiffractor=OFF \
+      -DINSTALL_TESTS=OFF \
+      -DSRW_OPTIMIZED=ON \
+      -DDEVELOPER_INSTALL=OFF \
+      -DCMAKE_INSTALL_PREFIX=$ROOT_DIR \
+      $ROOT_DIR \
+      ..
 
 chmod og+rwX -R $ROOT_DIR
 
 # Build the project.
 make
 
+echo "######## done make"
 
 # Install the project.
 make install
 cd ../..
 
-rm -rf simex_platform-${BRANCH}
+echo "####### done make install"
+
+#rm -rf simex_platform-${BRANCH}
 
 
 #remove tests?
-rm -rf $ROOT_DIR/Tests
+#rm -rf $ROOT_DIR/Tests
 
 echo "source /opt/simex_platform/bin/simex_vars.sh" > /etc/profile.d/scripts-simex.sh && \
 	chmod 755 /etc/profile.d/scripts-simex.sh
