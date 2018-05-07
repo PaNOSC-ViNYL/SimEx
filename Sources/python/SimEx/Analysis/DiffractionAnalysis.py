ba51e5f6ecbb845454378e6ded71bf4ff5964958
Carsten Fortmann-Grote <carsten.grote@xfel.eu>
2018-05-07 11:43:16 +0200

    Fixing for py3.

--- Sources/python/SimEx/Analysis/DiffractionAnalysis.py
+++ Sources/python/SimEx/Analysis/DiffractionAnalysis.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python2.7
+#!/usr/bin/env python
 """ :module DiffractionAnalysis: Module that hosts the DiffractionAnalysis class.  """
 ##########################################################################
 #                                                                        #
