f3b906f8735a8a8da04824a2e73b235013f24da2
Carsten Fortmann-Grote <carsten.grote@xfel.eu>
2018-07-10 07:43:19 +0200

    Fixing travis build script.

--- TravisCI/build_and_test_simex.sh
+++ TravisCI/build_and_test_simex.sh
@@ -7,7 +7,7 @@ git lfs pull
 # build & install
 mkdir build
 cd build
-cmake -DCMAKE_INSTALL_PREFIX=$VIRTUAL_ENV -DSRW_OPTIMIZED=ON -DXCSITPhotonDetector=OFF -DDEVELOPER_INSTALL=OFF ..
+cmake -DCMAKE_INSTALL_PREFIX=$VIRTUAL_ENV -DSRW_OPTIMIZED=ON -DUSE_XCSITPhotonDetector=OFF -DDEVELOPER_INSTALL=OFF ..
 make -j8
 make install
 
