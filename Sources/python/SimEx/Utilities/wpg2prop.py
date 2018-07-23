f583d1ab2c34991dc582f4cf3c666909f7215332
Carsten Fortmann-Grote <carsten.grote@xfel.eu>
2018-07-23 12:56:58 +0200

    Small fix.

--- Sources/python/SimEx/Utilities/wpg2prop.py
+++ Sources/python/SimEx/Utilities/wpg2prop.py
@@ -1,8 +1,9 @@
 """ Convert wpg out to prop out by inserting misc/xFWHM and misc/yFWHM datasets. """
 
-import sys
-import shutil
 import h5py
+import os
+import shutil
+import sys
 from wpg import Wavefront, wpg_uti_wf
 
 
@@ -11,6 +12,8 @@ def main(wpg_out):
     # Backup
     backup = wpg_out+".backup"
     shutil.copy2(wpg_out, backup)
+
+    print("Processing %s." % (wpg_out))
 
     # Load wavefront.
     wavefront = Wavefront()
@@ -28,11 +31,11 @@ def main(wpg_out):
 
     # If anything went wrong, restore the original file and raise.
     except:
-        shutil.move(backup,wpg_out)
+        shutil.move(backup, wpg_out)
         raise
 
     # We only reach this point if everything went well, so can safely remove the backup."
-    shutil.remove(backup)
+    os.remove(backup)
 
 if __name__ == "__main__":
     main(sys.argv[1])
