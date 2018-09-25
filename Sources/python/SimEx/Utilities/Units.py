b9a75ab050700e3fa3787a65506543b5a2cb36f9
Carsten Fortmann-Grote <carsten.grote@xfel.eu>
2018-09-25 21:34:33 +0200

    More units and constants.

--- Sources/python/SimEx/Utilities/Units.py
+++ Sources/python/SimEx/Utilities/Units.py
@@ -1,5 +1,8 @@
 from SimEx import PhysicalQuantity, ureg
-from scipy.constants import e,c,hbar
+
+from scipy import constants
+from scipy.constants import e,c,hbar, m_e, epsilon_0
+from scipy.constants import k as kB
 
 # Base units.
 meter           = PhysicalQuantity(1.0, 'meter')
@@ -13,3 +16,21 @@ kelvin          = PhysicalQuantity(1.0, 'kelvin')
 radian          = PhysicalQuantity(1.0, 'radian')
 electronvolt    = PhysicalQuantity(1.0, 'eV')
 coulomb         = PhysicalQuantity(1.0, 'C')
+farad           = PhysicalQuantity(1.0, 'farad')
+rydberg         = constants.value('Rydberg constant times hc in eV') * electronvolt
+bohr            = constants.value('Bohr radius') * meter
+angstromstar    = constants.value('Angstrom star') * meter
+angstrom        = 1.e-10 * meter
+radian          = PhysicalQuantity(1.0, 'radian')
+one             = PhysicalQuantity(1.0, 'dimensionless')
+
+e = e*coulomb
+c = c*meter/second
+hbar = hbar * joule * second
+m_e = m_e * kilogram
+epsilon0 = epsilon_0*farad/meter
+kB = kB*joule/kelvin
+
+def compatible(quant1, quant2):
+    return quant1.dimensionality == quant2.dimensionality
+
