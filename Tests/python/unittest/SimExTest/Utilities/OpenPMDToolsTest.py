4f92609f98df726425e597d3e1fd648593d95ac2
Carsten Fortmann-Grote <carsten.grote@xfel.eu>
2019-06-18 18:58:35 +0200

    Intermediate commit before switching branch.

--- Tests/python/unittest/SimExTest/Utilities/OpenPMDToolsTest.py
+++ Tests/python/unittest/SimExTest/Utilities/OpenPMDToolsTest.py
@@ -55,7 +55,7 @@ class OpenPMDToolsTest(unittest.TestCase):
                 os.remove(f)
 
     def testWpgToOPMDLegacyConverter(self):
-        """ Test the conversion of wpg output to openPMD conform file, omitting the openpmd-api."""
+        """ Test the conversion of wpg output to openPMD conform file."""
 
         # Get sample file.
         h5_input = generateTestFilePath('prop_out/prop_out_0000011.h5')
@@ -91,70 +91,34 @@ class OpenPMDToolsTest(unittest.TestCase):
         """ Test the conversion of wpg output to openPMD conform file."""
 
         # Get sample file.
-        h5_input = generateTestFilePath('prop_out/prop_out_0000011.h5')
+        h5_input = generateTestFilePath('prop_out_0000001.h5')
 
         # Convert.
         convertToOPMD(h5_input)
 
         # New file name.
         opmd_h5_file = h5_input.replace(".h5", ".opmd.h5")
+
+        # Make sure we clean up after test.
         self.__files_to_remove.append(opmd_h5_file)
 
         # Check new file was generated.
         self.assertTrue( os.path.isfile( opmd_h5_file ) )
 
-        # Read the file back in through the API.
-        series = opmd.Series(opmd_h5_file, opmd.Access_Type.read_only)
+        # Validate the new file.
+        g = opmd_validator.open_file(opmd_h5_file)
 
-        self.assertIsInstance(series, opmd.Series)
+        # Setup result array.
+        result_array = numpy.array([0, 0])
+        result_array += opmd_validator.check_root_attr(g, False)
 
-        # Check attributes are present.
-        try:
-            series.author
-            series.date
-            series.software
-            series.software_version
-            series.get_attribute("radius of curvature in x")
-            series.get_attribute("z coordinate")
-            series.get_attribute("Rx_Unit_Dimension")
-            series.get_attribute("Rx_UnitSI")
-            series.get_attribute("radius of curvature in y")
-            series.get_attribute("Ry_Unit_Dimension")
-            series.get_attribute("Ry_UnitSI")
-            series.get_attribute("Delta radius of curvature in x")
-            series.get_attribute("DRx_Unit_Dimension")
-            series.get_attribute("DRx_UnitSI")
-            series.get_attribute("Delta radius of curvature in y")
-            series.get_attribute("DRy_Unit_Dimension")
-            series.get_attribute("DRy_UnitSI")
-            series.get_attribute("photon energy")
-            series.get_attribute("photon energy unit dimension")
-            series.get_attribute("photon energy UnitSI")
+        # Go through all the iterations, checking both the particles and the meshes
+        extensions = {'ED-PIC': False, 'HYDRO1D': False}
+        result_array += opmd_validator.check_iterations(g,False,extensions)
 
-        except RuntimeError:
-            self.fail("Error while querying attribute.")
-        except:
-            raise
-
-        # Check the beamline serialization
-        self.assertIsInstance(series.get_attribute("beamline"), str)
-
-    def testLoadOPMDWavefront(self):
-        """ Test if loading a wavefront from openpmd-hdf into a WPG structure works."""
-
-        # Get sample file.
-        h5_input = generateTestFilePath('prop_out/prop_out_0000011.h5')
-
-        # Convert.
-        convertToOPMD(h5_input)
-
-        # New file name.
-        opmd_h5_file = h5_input.replace(".h5", ".opmd.h5")
-        self.__files_to_remove.append(opmd_h5_file)
-        
-        # Reconstruct the series.
-        series = opmd.Series(opmd_h5_file, opmd.Access_Type.read_only)
-        wavefront = wpg.Wavefront()
+        # Assert that no errors nor warnings were issued.
+        self.assertEqual( result_array[0], 0 )
+        self.assertEqual( result_array[1], 0 )
 
     def testHydroTxtToOPMDConverter(self):
         """ Test the conversion of esther output to openPMD conform hdf5 file."""
