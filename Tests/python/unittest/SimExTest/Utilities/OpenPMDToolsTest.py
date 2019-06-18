76e6664998d85c88d95fb79c224a65faf7b9b84f
Carsten Fortmann-Grote <carsten.grote@xfel.eu>
2019-06-18 18:58:35 +0200

    Intermediate commit before switching branch.

--- Tests/python/unittest/SimExTest/Utilities/OpenPMDToolsTest.py
+++ Tests/python/unittest/SimExTest/Utilities/OpenPMDToolsTest.py
@@ -56,7 +56,7 @@ class OpenPMDToolsTest(unittest.TestCase):
             if os.path.isfile(f):
                 os.remove(f)
 
-    def testWpgToOPMDConverter(self):
+    def testWpgToOPMDLegacyConverter(self):
         """ Test the conversion of wpg output to openPMD conform file."""
 
         # Get sample file.
@@ -89,6 +89,38 @@ class OpenPMDToolsTest(unittest.TestCase):
         self.assertEqual( result_array[0], 0 )
         self.assertEqual( result_array[1], 0 )
 
+    def testWpgToOPMDConverter(self):
+        """ Test the conversion of wpg output to openPMD conform file."""
+
+        # Get sample file.
+        h5_input = generateTestFilePath('prop_out_0000001.h5')
+
+        # Convert.
+        convertToOPMD(h5_input)
+
+        # New file name.
+        opmd_h5_file = h5_input.replace(".h5", ".opmd.h5")
+
+        # Make sure we clean up after test.
+        self.__files_to_remove.append(opmd_h5_file)
+
+        # Check new file was generated.
+        self.assertTrue( os.path.isfile( opmd_h5_file ) )
+
+        # Validate the new file.
+        g = opmd_validator.open_file(opmd_h5_file)
+
+        # Setup result array.
+        result_array = numpy.array([0, 0])
+        result_array += opmd_validator.check_root_attr(g, False)
+
+        # Go through all the iterations, checking both the particles and the meshes
+        extensions = {'ED-PIC': False, 'HYDRO1D': False}
+        result_array += opmd_validator.check_iterations(g,False,extensions)
+
+        # Assert that no errors nor warnings were issued.
+        self.assertEqual( result_array[0], 0 )
+        self.assertEqual( result_array[1], 0 )
 
     def testHydroTxtToOPMDConverter(self):
         """ Test the conversion of esther output to openPMD conform hdf5 file."""
