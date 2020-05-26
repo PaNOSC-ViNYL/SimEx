c8ea0a331682ad3401890215a6643481d6dab24f
Juncheng E <juncheng.e@xfel.eu>
2020-05-26 15:17:17 +0200

    add solidAngles mapping

--- Sources/python/SimEx/Analysis/DiffractionAnalysis.py
+++ Sources/python/SimEx/Analysis/DiffractionAnalysis.py
@@ -135,12 +135,13 @@ class DiffractionAnalysis(AbstractAnalysis):
     @property
     def solidAngles(self):
         """ Solid angle of each pixel """
+        """ Note: the pixel is assumed to be square """
 
         # pixel number (py, px)
-        pn = analyzer.parameters['geom']['mask'].shape
+        pn = self.parameters['geom']['mask'].shape
         # initialize array
         solidAngles = numpy.zeros_like(pn)
-        y, x = np.indices(pn)
+        y, x = numpy.indices(pn)
         # pixel size (meter)
         ph = self.parameters['geom']['pixelHeight']
         pw = self.parameters['geom']['pixelWidth']
@@ -151,14 +152,41 @@ class DiffractionAnalysis(AbstractAnalysis):
         center_y = 0.5*(pn[0]-1)
         rx = (x - center_x)*pw
         ry = (y - center_y)*ph
-        r = np.sqrt(rx**2 + ry**2)
+        r = numpy.sqrt(rx**2 + ry**2)
+        pixDist = numpy.sqrt(r**2 + s2d**2)
+        alpha = numpy.arctan2(pw,2*pixDist)
+        solidAngles = 4*numpy.arcsin(numpy.sin(alpha)**2)
 
-        if (self.pattern_indices == "all"):
-            with h5py.File(self.input_path, 'r') as h5:
-                npattern = len(h5['data'])
-        else:
-            npattern = len(self.pattern_indices)
-        return npattern
+        return solidAngles
+
+    @property
+    def qMap(self):
+        """ q of each pixel """
+        """ q = 4*pi*sin(twotheta/2)/lmd """
+
+        # pixel number (py, px)
+        pn = self.parameters['geom']['mask'].shape
+        # initialize array
+        qMap = numpy.zeros_like(pn)
+        y, x = numpy.indices(pn)
+        # pixel size (meter)
+        ph = self.parameters['geom']['pixelHeight']
+        pw = self.parameters['geom']['pixelWidth']
+        # sample to detector distance (meter)
+        s2d = self.parameters['geom']['detectorDist']
+
+        E0 = self.parameters['beam']['photonEnergy']
+        lmd = 12398 / E0 #Angstrom
+
+        center_x = 0.5*(pn[1]-1)
+        center_y = 0.5*(pn[0]-1)
+        rx = (x - center_x)*pw
+        ry = (y - center_y)*ph
+        r = numpy.sqrt(rx**2 + ry**2)
+        twotheta = numpy.arctan2(r,s2d)
+        qMap = 4*numpy.pi*numpy.sin(twotheta/2)/lmd
+
+        return qMap
 
     @property
     def mask(self):
@@ -496,6 +524,8 @@ def plotImage(pattern, logscale=False, offset=1e-1):
     plt.ylim([0,y_range-1])
     plt.tight_layout()
     plt.colorbar()
+
+
 
 def plotResolutionRings(parameters):
     """
