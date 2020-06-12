2186d287083da0a786158596c2bda51968c95d01
Carsten Fortmann-Grote <carsten.grote@xfel.eu>
2020-06-12 22:29:48 +0200

    Adding testdata for TNSAIonMatterInteractorTest.

--- get_testdata.sh
+++ get_testdata.sh
@@ -4,4 +4,7 @@ wget https://zenodo.org/record/3750541/files/simex_testdata?download=1 -O - | gu
 cd  Tests/python/unittest
 tar -xvf TestFiles.tar
 rm -v TestFiles.tar 
+wget https://github.com/PaNOSC-ViNYL/neutrontools/raw/master/Data/0010.sdf
+wget https://github.com/PaNOSC-ViNYL/neutrontools/raw/master/Data/D_D_-_3He_n.txt
+mv -v 0010.sdf D_D_-_3He_n.txt TestFiles
 cd -
