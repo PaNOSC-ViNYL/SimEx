18a949aa580da11204c342dc8fcaef1b590d8a0f
Juncheng E <ejcjason@gmail.com>
2020-08-03 16:30:29 +0200

    Replace the deprecated stickysidebar with sticky_navigation

--- Sources/doc/source/conf.py
+++ Sources/doc/source/conf.py
@@ -120,7 +120,8 @@ html_theme = 'sphinx_rtd_theme'
 # further.  For a list of options available for each theme, see the
 # documentation.
 html_theme_options = {
-        "stickysidebar" : "true",
+        # "stickysidebar" : "true",
+        "sticky_navigation" : "true",
         }
 
 # Add any paths that contain custom themes here, relative to this directory.
