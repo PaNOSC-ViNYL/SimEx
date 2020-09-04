32899c5fa4c8666dd35b5e09fea91ace8cf07a89
Carsten Fortmann-Grote <carsten.grote@xfel.eu>
2020-09-04 08:43:23 +0200

    Fix Gauss parameters divergence and beam_diameter_fwhm to be consistent.

--- Sources/python/SimEx/Parameters/GaussWavefrontParameters.py
+++ Sources/python/SimEx/Parameters/GaussWavefrontParameters.py
@@ -25,6 +25,7 @@ from SimEx.Parameters.PhotonBeamParameters import PhotonBeamParameters
 from SimEx.Utilities.EntityChecks import checkAndSetInstance, checkAndSetPhysicalQuantity
 from SimEx.Utilities.Units import meter, electronvolt, joule, radian
 
+from scipy.constants import hbar, c
 from scipy import constants
 from wpg import Wavefront, wpg_uti_wf
 from wpg.srwlib import srwl
@@ -35,13 +36,13 @@ import sys
 
 class GaussWavefrontParameters(PhotonBeamParameters):
     def __init__(self,
-            photon_energy,
-            beam_diameter_fwhm,
-            pulse_energy,
-            photon_energy_relative_bandwidth,
-            divergence,
-            number_of_transverse_grid_points,
-            number_of_time_slices,
+            photon_energy=None,
+            pulse_energy=None,
+            photon_energy_relative_bandwidth=None,
+            number_of_transverse_grid_points=None,
+            number_of_time_slices=None,
+            beam_diameter_fwhm=None,
+            divergence=None,
             **kwargs
             ):
         """
@@ -76,6 +77,23 @@ class GaussWavefrontParameters(PhotonBeamParameters):
 
         """
 
+        if (beam_diameter_fwhm is not None and divergence is not None) or (beam_diameter_fwhm is None and divergence is None):
+            raise ValueError("beam_diameter_fwhm and divergence cannot be set\
+            at the same time. Chose one and leave the other one None or\
+            undefined.")
+
+        if divergence is None:
+            divergence = get_divergence_from_beam_diameter(
+                    photon_energy,
+                    beam_diameter_fwhm
+                    )
+            
+        elif beam_diameter_fwhm is None:
+            beam_diameter_fwhm = get_beam_diameter_from_divergence(
+                    photon_energy,
+                    divergence,
+                    )
+
         super().__init__(
                 photon_energy=photon_energy,
                 beam_diameter_fwhm=beam_diameter_fwhm,
@@ -88,6 +106,7 @@ class GaussWavefrontParameters(PhotonBeamParameters):
         
         self.number_of_transverse_grid_points = number_of_transverse_grid_points
         self.number_of_time_slices = number_of_time_slices
+        
 
     def _setDefaults(self):
         """ Set default for required inherited parameters. """
@@ -120,5 +139,25 @@ class GaussWavefrontParameters(PhotonBeamParameters):
             raise ValueError('The parameter "number_of_time_slices" must\
                              be at least 3.')
         self.__number_of_time_slices = val
+
+def get_divergence_from_beam_diameter(E, beam_diameter_fwhm):
+    """ Calculate the beam diameter fwhm from the divergence angle. """
+    # The rms of the amplitude distribution (Gaussian)
+    beam_waist = 2.*beam_diameter_fwhm.m_as(meter)*math.sqrt(math.log(2.))
+    theta = 2.*hbar*c/beam_waist/E.m_as(joule)
+    
+    return theta*radian
+
+
+def get_beam_diameter_from_divergence(E, divergence):
+    """ Calculate the bandwith fwhm from the divergence angle. """
+    # The rms of the amplitude distribution (Gaussian)
+    theta = divergence.m_as(radian)
+
+    beam_waist = 2.*hbar*c/theta/E.m_as(joule)
+    intensity_fwhm = 0.5*beam_waist/math.sqrt(math.log(2.))*meter
+
+    return intensity_fwhm
+    
 
 
